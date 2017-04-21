7333b74c165f4238c36d300584bc9f76e344f5d7
David Kotz <kotz@cs.dartmouth.edu>
2017-04-21 17:59:37 -0400

    add webpage_internal.h

---
+++ libcs50/webpage_internal.h
@@ -0,0 +1,24 @@
+/* 
+ * webpage_internal.h - glue between webpage.c and webpage_fetch.c
+ *
+ * David Kotz - April 2017
+ *
+ */
+
+#ifndef __WEBPAGE_INTERNAL_H
+#define __WEBPAGE_INTERNAL_H
+
+#include <stdlib.h>
+
+/***********************************************************************/
+/* webpage_t: structure to represent a web page, and its contents.
+ * The innards should not be visible to users of the webpage module.
+ */
+typedef struct webpage {
+  char *url;                               // url of the page
+  char *html;                              // html code of the page
+  size_t html_len;                         // length of html code
+  int depth;                               // depth of crawl
+} webpage_t;
+
+#endif // __WEBPAGE_INTERNAL_H
