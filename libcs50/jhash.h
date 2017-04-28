f0312ec95c625e357d9b3fbf4d398a2a0ef7e25d
David Kotz <kotz@cs.dartmouth.edu>
2017-04-28 05:36:22 -0400

    add defensive argument check, bring style in line with CS50.

--- libcs50/jhash.h
+++ libcs50/jhash.h
@@ -1,6 +1,8 @@
 /* =========================================================================
  * jhash.h - Jenkins' Hash, maps from string to integer
  *
+ * Implementation details can be found at:
+ *     http://www.burtleburtle.net/bob/hash/doobs.html
  * ========================================================================= 
  */
 
@@ -9,12 +11,10 @@
 
 /*
  * jenkins_hash - Bob Jenkins' one_at_a_time hash function
- * @str: char buffer to hash
- * @mod: desired hash modulus
+ * @str: char buffer to hash (non-NULL)
+ * @mod: desired hash modulus (>0)
  *
- * Returns hash(str) % mod. Depends on str being null terminated.
- * Implementation details can be found at:
- *     http://www.burtleburtle.net/bob/hash/doobs.html
+ * Returns hash(str) % mod. 
  */
 unsigned long JenkinsHash(const char *str, unsigned long mod);
 
